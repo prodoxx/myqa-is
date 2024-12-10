423d2252a7f18451d35f3d045ef5d156321d1e8e
Reggie <reggie.escobar94@gmail.com>
2024-12-11 00:39:08 +0800

    chore: upgraded prisma and added encryption extention

--- app/app/infrastructure/database/index.server.ts
+++ app/app/infrastructure/database/index.server.ts
@@ -1,17 +1,31 @@
-// import type { Ekyash as EkyashORM, Order as OrderORM, User as UserORM } from '@prisma/client';
-// import { Currency, OrderStatus, PrismaClient } from '@prisma/client';
 import { PrismaClient } from '@prisma/client';
+import { fieldEncryptionExtension } from 'prisma-field-encryption';
 
 // add prisma to the NodeJS global type
 interface CustomNodeJsGlobal extends NodeJS.Global {
-  prisma: PrismaClient;
+  prisma: ReturnType<typeof createPrismaClient>;
 }
 
 // Prevent multiple instances of Prisma Client in development
 declare const global: CustomNodeJsGlobal;
 
-const prisma =
-  global.prisma || new PrismaClient({ datasources: { db: { url: process.env.POSTGRES_PRISMA_URL as string } } });
+const createPrismaClient = () => {
+  const prisma = new PrismaClient({
+    datasources: {
+      db: {
+        url: process.env.POSTGRES_PRISMA_URL as string,
+      },
+    },
+  });
+
+  return prisma.$extends(
+    fieldEncryptionExtension({
+      encryptionKey: process.env.ENCRYPTION_KEY as string,
+    })
+  );
+};
+
+const prisma = global.prisma || createPrismaClient();
 
 if (process.env.NODE_ENV === 'development') global.prisma = prisma;
 
