35b9640f2c51314a5e91b71ddb55c9eb3859e951
Derrick Bol <derrickbol33@gmail.com>
2024-12-14 14:16:09 +0800

    fix: envs

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -84,56 +84,68 @@ export default ({ mode }) => {
       },
     },
     define: {
-      'process.env.DATABASE_URL': String(process.env.DATABASE_URL),
-      'process.env.POSTGRES_PRISMA_URL': String(
+      'process.env.DATABASE_URL': JSON.stringify(process.env.DATABASE_URL),
+      'process.env.POSTGRES_PRISMA_URL': JSON.stringify(
         process.env.POSTGRES_PRISMA_URL
       ),
-      'process.env.REDIS_URL': String(process.env.REDIS_URL),
-      'process.env.PASSWORD_COOKIE_SECRET': String(
+      'process.env.REDIS_URL': JSON.stringify(process.env.REDIS_URL),
+      'process.env.PASSWORD_COOKIE_SECRET': JSON.stringify(
         process.env.PASSWORD_COOKIE_SECRET
       ),
-      'process.env.AWS_S3_KEY_ID': String(process.env.AWS_S3_KEY_ID),
-      'process.env.AWS_S3_KEY_SECRET': String(process.env.AWS_S3_KEY_SECRET),
-      'process.env.GOOGLE_CLIENT_ID': String(process.env.GOOGLE_CLIENT_ID),
-      'process.env.GOOGLE_CLIENT_SECRET': String(
+      'process.env.AWS_S3_KEY_ID': JSON.stringify(process.env.AWS_S3_KEY_ID),
+      'process.env.AWS_S3_KEY_SECRET': JSON.stringify(
+        process.env.AWS_S3_KEY_SECRET
+      ),
+      'process.env.GOOGLE_CLIENT_ID': JSON.stringify(
+        process.env.GOOGLE_CLIENT_ID
+      ),
+      'process.env.GOOGLE_CLIENT_SECRET': JSON.stringify(
         process.env.GOOGLE_CLIENT_SECRET
       ),
-      'process.env.GOOGLE_CLIENT_CALLBACK_URL': String(
+      'process.env.GOOGLE_CLIENT_CALLBACK_URL': JSON.stringify(
         process.env.GOOGLE_CLIENT_CALLBACK_URL
       ),
-      'process.env.DIGITAL_OCEAN_ENDPOINT_URL': String(
+      'process.env.DIGITAL_OCEAN_ENDPOINT_URL': JSON.stringify(
         process.env.DIGITAL_OCEAN_ENDPOINT_URL
       ),
-      'process.env.DIGITAL_OCEAN_REGION': String(
+      'process.env.DIGITAL_OCEAN_REGION': JSON.stringify(
         process.env.DIGITAL_OCEAN_REGION
       ),
-      'process.env.DIGITAL_OCEAN_API_ID': String(
+      'process.env.DIGITAL_OCEAN_API_ID': JSON.stringify(
         process.env.DIGITAL_OCEAN_API_ID
       ),
-      'process.env.DIGITAL_OCEAN_API_KEY': String(
+      'process.env.DIGITAL_OCEAN_API_KEY': JSON.stringify(
         process.env.DIGITAL_OCEAN_API_KEY
       ),
-      'process.env.DIGITAL_OCEAN_BUCKET': String(
+      'process.env.DIGITAL_OCEAN_BUCKET': JSON.stringify(
         process.env.DIGITAL_OCEAN_BUCKET
       ),
-      'process.env.BINANCE_API_KEY': String(process.env.BINANCE_API_KEY),
-      'process.env.BINANCE_API_SECRET': String(process.env.BINANCE_API_SECRET),
-      'process.env.BETTER_STACK_LOGS': String(process.env.BETTER_STACK_LOGS),
-      'process.env.PINATA_API_KEY': String(process.env.PINATA_API_KEY),
-      'process.env.PINATA_SECRET_KEY': String(process.env.PINATA_SECRET_KEY),
-      'process.env.PINATA_JWT': String(process.env.PINATA_JWT),
-      'process.env.PRISMA_FIELD_ENCRYPTION_KEY': String(
+      'process.env.BINANCE_API_KEY': JSON.stringify(
+        process.env.BINANCE_API_KEY
+      ),
+      'process.env.BINANCE_API_SECRET': JSON.stringify(
+        process.env.BINANCE_API_SECRET
+      ),
+      'process.env.BETTER_STACK_LOGS': JSON.stringify(
+        process.env.BETTER_STACK_LOGS
+      ),
+      'process.env.PINATA_API_KEY': JSON.stringify(process.env.PINATA_API_KEY),
+      'process.env.PINATA_SECRET_KEY': JSON.stringify(
+        process.env.PINATA_SECRET_KEY
+      ),
+      'process.env.PINATA_JWT': JSON.stringify(process.env.PINATA_JWT),
+      'process.env.PRISMA_FIELD_ENCRYPTION_KEY': JSON.stringify(
         process.env.PRISMA_FIELD_ENCRYPTION_KEY
       ),
-      'process.env.POSTHOG_URL': String(process.env.POSTHOG_URL),
-      'process.env.MARKETPLACE_PROGRAM_ID': String(
+      'process.env.POSTHOG_URL': JSON.stringify(process.env.POSTHOG_URL),
+      'process.env.MARKETPLACE_PROGRAM_ID': JSON.stringify(
         process.env.MARKETPLACE_PROGRAM_ID
       ),
-      'process.env.MARKETPLACE_AUTHORITY_PUBLIC_KEY': String(
+      'process.env.MARKETPLACE_AUTHORITY_PUBLIC_KEY': JSON.stringify(
         process.env.MARKETPLACE_AUTHORITY_PUBLIC_KEY
       ),
-      'process.env.SOLANA_NETWORK': String(process.env.SOLANA_NETWORK),
-      'process.env.SOLANA_RPC_URL': String(process.env.SOLANA_RPC_URL),
+      'process.env.SOLANA_NETWORK': JSON.stringify(process.env.SOLANA_NETWORK),
+      'process.env.SOLANA_RPC_URL': JSON.stringify(process.env.SOLANA_RPC_URL),
     },
   });
 };
