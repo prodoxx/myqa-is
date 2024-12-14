dc060ca21ed26baaba75ec999988a05c1bb7dfb8
Derrick Bol <derrickbol33@gmail.com>
2024-12-14 14:08:26 +0800

    fix: values

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -9,6 +9,10 @@ import { vercelPreset } from '@vercel/remix/vite';
 installGlobals({ nativeFetch: true });
 
 export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  console.log(process.env);
+
   return defineConfig({
     ssr: {
       noExternal:
@@ -78,6 +82,35 @@ export default ({ mode }) => {
       supported: {
         'top-level-await': true, //browsers can handle top-level-await features
       },
+    },
+    define: {
+      DATABASE_URL: process.env.DATABASE_URL,
+      POSTGRES_PRISMA_URL: process.env.POSTGRES_PRISMA_URL,
+      REDIS_URL: process.env.REDIS_URL,
+      PASSWORD_COOKIE_SECRET: process.env.PASSWORD_COOKIE_SECRET,
+      AWS_S3_KEY_ID: process.env.AWS_S3_KEY_ID,
+      AWS_S3_KEY_SECRET: process.env.AWS_S3_KEY_SECRET,
+      GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+      GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+      GOOGLE_CLIENT_CALLBACK_URL: process.env.GOOGLE_CLIENT_CALLBACK_URL,
+      DIGITAL_OCEAN_ENDPOINT_URL: process.env.DIGITAL_OCEAN_ENDPOINT_URL,
+      DIGITAL_OCEAN_REGION: process.env.DIGITAL_OCEAN_REGION,
+      DIGITAL_OCEAN_API_ID: process.env.DIGITAL_OCEAN_API_ID,
+      DIGITAL_OCEAN_API_KEY: process.env.DIGITAL_OCEAN_API_KEY,
+      DIGITAL_OCEAN_BUCKET: process.env.DIGITAL_OCEAN_BUCKET,
+      BINANCE_API_KEY: process.env.BINANCE_API_KEY,
+      BINANCE_API_SECRET: process.env.BINANCE_API_SECRET,
+      BETTER_STACK_LOGS: process.env.BETTER_STACK_LOGS,
+      PINATA_API_KEY: process.env.PINATA_API_KEY,
+      PINATA_SECRET_KEY: process.env.PINATA_SECRET_KEY,
+      PINATA_JWT: process.env.PINATA_JWT,
+      PRISMA_FIELD_ENCRYPTION_KEY: process.env.PRISMA_FIELD_ENCRYPTION_KEY,
+      POSTHOG_URL: process.env.POSTHOG_URL,
+      MARKETPLACE_PROGRAM_ID: process.env.MARKETPLACE_PROGRAM_ID,
+      MARKETPLACE_AUTHORITY_PUBLIC_KEY:
+        process.env.MARKETPLACE_AUTHORITY_PUBLIC_KEY,
+      SOLANA_NETWORK: process.env.SOLANA_NETWORK,
+      SOLANA_RPC_URL: process.env.SOLANA_RPC_URL,
     },
   });
 };
